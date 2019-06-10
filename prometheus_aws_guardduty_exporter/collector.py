f94e6f29db7616450580d6e2be6d7c43049f0a47
Esteban Sánchez <esteban.sanchez@spreaker.com>
2019-06-10 18:51:10 +0200

    Use ThreadPools instead of threading and a lock

--- prometheus_aws_guardduty_exporter/collector.py
+++ prometheus_aws_guardduty_exporter/collector.py
@@ -1,7 +1,7 @@
 import logging
 import boto3
 import botocore
-import threading
+from multiprocessing.dummy import Pool
 from typing import List
 from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily
 
@@ -9,26 +9,7 @@ from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily
 class GuardDutyMetricsCollector():
     def __init__(self, regions: List[str]):
         self.regions = regions
-        self.scrapeErrors = {region: 0 for region in regions}
-        self.botoConfig = None
-        self.lock = threading.Lock()
-
-    def scrape_metric_in_region(self, metric, region):
-        try:
-            # Scrape GuardDuty statistics from the region
-            regionStats = self._collectMetricsByRegion(region)
-            # Add the findings to the exporterd metric
-            for severity, count in regionStats.items():
-                self.lock.acquire()
-                try:
-                    metric.add_metric(value=count, labels=[region, severity])
-                finally:
-                    self.lock.release()
-        except Exception as error:
-            logging.getLogger().error(f"Unable to scrape GuardDuty statistics from {region} because of error: {str(error)}")
-
-            # Increase the errors count
-            self.scrapeErrors[region] += 1
+        self.botoConfig = botocore.client.Config(connect_timeout=2, read_timeout=10, retries={"max_attempts": 2})
 
     def collect(self):
         # Init metrics
@@ -42,51 +23,51 @@ class GuardDutyMetricsCollector():
             "The total number of scrape errors",
             labels=["region"])
 
-        # Customize the boto client config
-        self.botoConfig = botocore.client.Config(connect_timeout=2, read_timeout=10, retries={"max_attempts": 2})
-
-        threads = []
-        for region in self.regions:
-            # We start one thread per region.
-            process = threading.Thread(target=self.scrape_metric_in_region, args=[currentFindingsMetric, region])
-            threads.append(process)
-
-        for process in threads:
-            process.start()
-
-        for process in threads:
-            process.join()
-
-        # Update the scrape errors metric
-        for region, errorsCount in self.scrapeErrors.items():
-            scrapeErrorsMetric.add_metric(value=errorsCount, labels=[region])
+        pool = Pool(len(self.regions))
+        results = [pool.apply_async(self._collectMetricsByRegion, [region]) for region in self.regions]
+        # results = pool.apply_async(self._collectMetricsByRegion, [region])
+        for result in results:
+            region, regionStats, scrapeErrors = result.get()
+            scrapeErrorsMetric.add_metric(value=scrapeErrors, labels=[region])
+            for severity, count in regionStats.items():
+                currentFindingsMetric.add_metric(value=count, labels=[region, severity])
 
         return [currentFindingsMetric, scrapeErrorsMetric]
 
     def _collectMetricsByRegion(self, region):
         client = boto3.client("guardduty", config=self.botoConfig, region_name=region)
         regionStats = {"low": 0, "medium": 0, "high": 0}
+        scrapeErrors = 0
 
-        # List GuardDuty detectors
-        detectorIds = client.list_detectors()["DetectorIds"]
+        try:
+            # List GuardDuty detectors
+            detectorIds = client.list_detectors()["DetectorIds"]
 
-        # Get statistics
-        for detectorId in detectorIds:
-            countBySeverity = client.get_findings_statistics(
-                DetectorId=detectorId,
-                FindingCriteria={"Criterion": {"service.archived": {"Eq": ["false"]}}},
-                FindingStatisticTypes=["COUNT_BY_SEVERITY"])["FindingStatistics"]["CountBySeverity"]
+            # Get statistics
+            for detectorId in detectorIds:
+                countBySeverity = client.get_findings_statistics(
+                    DetectorId=detectorId,
+                    FindingCriteria={"Criterion": {"service.archived": {"Eq": ["false"]}}},
+                    FindingStatisticTypes=["COUNT_BY_SEVERITY"])["FindingStatistics"]["CountBySeverity"]
 
-            for severity, count in countBySeverity.items():
-                severity = float(severity)
+                for severity, count in countBySeverity.items():
+                    severity = float(severity)
 
-                # Group severity levels into low, medium and high according to this doc:
-                # https://docs.aws.amazon.com/guardduty/latest/ug/guardduty_findings.html#guardduty_findings-severity
-                if severity < 4:
-                    regionStats["low"] += count
-                elif severity < 7:
-                    regionStats["medium"] += count
-                else:
-                    regionStats["high"] += count
+                    # Group severity levels into low, medium and high according to this doc:
+                    # https://docs.aws.amazon.com/guardduty/latest/ug/guardduty_findings.html#guardduty_findings-severity
+                    if severity < 4:
+                        regionStats["low"] += count
+                    elif severity < 7:
+                        regionStats["medium"] += count
+                    else:
+                        regionStats["high"] += count
+        except Exception as error:
+            logging.getLogger().error(f"Unable to scrape GuardDuty statistics from {region} because of error: {str(error)}")
 
-        return regionStats
+            # Increase the errors count
+            scrapeErrors = 1
+
+            # Do not return regionStats or they could be 0 and be a false positive
+            regionStats = {}
+
+        return (region, regionStats, scrapeErrors)
